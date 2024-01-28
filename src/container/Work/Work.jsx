4f31ae3955aa102c0908584b107e7035c54fc6b1
kiprono <im.kiprono@gmail.com>
2024-01-28 17:57:46 +0300

    filter tags

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -8,7 +8,24 @@ import { urlFor, client } from '../../client'
 
 const Work = () => {
 
-  const handleWorkFilter = (item) =>{}
+  const handleWorkFilter = (item) =>{
+    setActiveFilter(item);
+    setanimateCard([{y:100, opacity: 0}])
+
+    setTimeout(() => {
+      setanimateCard([{y:100, opacity: 0}])
+    })
+
+    setTimeout(() => {
+      setanimateCard([{y:0, opacity:1}]);
+      if( item === 'All'){
+        setfilterWork(works);
+
+      } else {
+        setfilterWork(works.filter((work) => work.tags.includes(item)))
+      }
+    }, 500);
+  }
   const [animateCard, setanimateCard] = useState({y:0, opacity: 1})
   const [works, setWorks] = useState([])
   const [filterWork, setfilterWork] = useState([])
@@ -48,54 +65,60 @@ useEffect(() => {
         </div>
       ))}
     </div>
+
+
     <motion.div
-    animate={animateCard}
-    transition={{ duration: 0.5, delayChildren: 0.5, }}
-    className='app__work-porfolio'
-    >
-      {filterWork.map((work, index) => (
-        <div className="app__work-item app__flex" key={index}>
-          <div className="app__work-img app__flex">
-            <img src={urlFor(work.imgUrl)} alt={work.name} /> 
-          </div>
-        </div>
-      ))}
-
-      <motion.div
-      whileHover={{opacity: [0, 1]}}
-      transition={{duration: 0.25, ease: 'easeInOut', staggerChildren: 0}}
-      className='app__work-hover app__flex'
-      
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-portfolio"
       >
+        {filterWork.map((work, index) => (
+          <div className="app__work-item app__flex" key={index}>
+            <div
+              className="app__work-img app__flex"
+            >
+              <img src={urlFor(work.imgUrl)} alt={work.name} />
 
+              <motion.div
+                whileHover={{ opacity: [0, 1] }}
+                transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+                className="app__work-hover app__flex"
+              >
+                <a href={work.projectLink} target="_blank" rel="noreferrer">
 
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillEye />
+                  </motion.div>
+                </a>
+                <a href={work.codeLink} target="_blank" rel="noreferrer">
+                  <motion.div
+                    whileInView={{ scale: [0, 1] }}
+                    whileHover={{ scale: [1, 0.90] }}
+                    transition={{ duration: 0.25 }}
+                    className="app__flex"
+                  >
+                    <AiFillGithub />
+                  </motion.div>
+                </a>
+              </motion.div>
+            </div>
 
-        <a href={works.projectLink} target='_blank' rel='noreferrer'>
-          <motion.div
-          whileInView={{ scale: [0, 1]}}
-          whileHover={{ scale: [1, 0.9]}}
-          transition={{ duration: 0.25 }}
-          className='app__flex'
-          >
-              <AiFillEye />
-          </motion.div>
-        </a>
+            <div className="app__work-content app__flex">
+              <h4 className="bold-text">{work.title}</h4>
+              <p className="p-text" style={{ marginTop: 10 }}>{work.description}</p>
 
-        <a href={works.projectLink} target='_blank' rel='noreferrer'>
-          <motion.div
-          whileInView={{ scale: [0, 1]}}
-          whileHover={{ scale: [1, 0.9]}}
-          transition={{ duration: 0.25 }}
-          className='app__flex'
-          >
-              <AiFillGithub />
-          </motion.div>
-        </a>
-
-
+              <div className="app__work-tag app__flex">
+                <p className="p-text">{work.tags[0]}</p>
+              </div>
+            </div>
+          </div>
+        ))}
       </motion.div>
-
-    </motion.div>
     </>
 
   
@@ -103,4 +126,4 @@ useEffect(() => {
   )
 }
 
-export default Work
+export default AppWrap(Work, 'work')
